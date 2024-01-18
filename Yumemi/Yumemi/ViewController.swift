d49f3e2fc0ff6e5d403212a9aca40a0c115d669d
school06 <>
2024-01-18 09:18:53 +0900

    初回コミット

---
+++ Yumemi/Yumemi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yumemi
+//
+//  Created by school06 on 2024/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
