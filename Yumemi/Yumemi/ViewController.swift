61b3b150a8775e426ca79a6140e2db6960f167c3
school06 <>
2024-01-18 09:42:02 +0900

    API完成

--- Yumemi/Yumemi/ViewController.swift
+++ Yumemi/Yumemi/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reloadButton(_ sender: Any) {
+        setWeatherImage()
+    }
+    
+    func setWeatherImage() {
+        let responseWeatherString = YumemiWeather.fetchWeatherCondition()
+        
+        var weatherName = "sunny"
+        var tintColor = UIColor.red
+        
+        switch responseWeatherString {
+        case "sunny":
+            weatherName = "sunny"
+            tintColor = UIColor.red
+        case "cloudy":
+            weatherName = "cloudy"
+            tintColor = UIColor.gray
+        case "rainy":
+            weatherName = "rainy"
+            tintColor = UIColor.blue
+        default:
+            break
+        }
+        weatherImage.image = UIImage(named: weatherName)
+        weatherImage.tintColor = tintColor
+        
+    }
 }
 
